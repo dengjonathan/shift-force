a96f6eeecc630e5110b9af971aff2c4cbe8625ed
jonathan.deng <dengjonathan@gmail.com>
2016-12-20 15:50:02 -0500

    feat (query): add promise based query

--- services/salesforce/query.js
+++ services/salesforce/query.js
@@ -1,10 +1,23 @@
-const salesforce = require('./connect');
-
-const query = queryString => {
-  if (!salesforce.loggedIn) {
-    return salesforce.login().then( _ => salesforce.conn.query(queryString));
-  }
-  return salesforce.conn.query(queryString);
+const jsforce = require('jsforce');
+const query = (queryString, accessToken, instanceUrl) => {
+  const conn = new jsforce.Connection({
+    accessToken,
+    instanceUrl
+  });
+  return new Promise((resolve, reject) => {
+    conn.query(queryString, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    });
+  });
 };
 
-exports.getUsers = (cb) => query('SELECT Name FROM Account');
+exports.getAccounts = (accessToken, instanceUrl) => query(
+  'SELECT id, name FROM account LIMIT 50',
+  accessToken,
+  instanceUrl
+);
+
+exports.getSeekers = () => query('SELECT id, name FROM seekers LIMIT 50');
