7ba41a229791915ff64d3b068ff3cb2a28c837c7
jonathan.deng <dengjonathan@gmail.com>
2016-12-20 16:09:48 -0500

    refactor (oauth): seperate SF login into its own module

--- server.js
+++ server.js
@@ -4,46 +4,42 @@ const session = require('express-session');
 const jsforce = require('jsforce');
 const passport = require('passport');
 const app = express();
-const oauth2 = require('./services/salesforce/oauth');
-const query = require('./services/salesforce/query');
+const SFConnect = require('./services/salesforce/oauth');
+const SFQuery = require('./services/salesforce/query');
 
 app.use(session({
   secret: 'supaSecret',
   resave: false,
-  saveUnitialized: true
+  saveUninitialized: true
 }));
 
 /* SF OAuth request, redirect to SF login */
-app.get('/oauth/auth', function(req, res) {
-    res.redirect(oauth2.getAuthorizationUrl({scope: 'api id web'}));
+app.get('/oauth/auth', function (req, res) {
+  res.redirect(SFConnect.oauth2.getAuthorizationUrl({
+    scope: 'api id web'
+  }));
 });
 
 /* OAuth callback from SF, pass received auth code and get access token */
-app.get('/oauth/callback', function(req, res) {
-    const conn = new jsforce.Connection({oauth2: oauth2});
-    var code = req.query.code;
-    conn.authorize(code, function(err, userInfo) {
-        if (err) { return console.error(err); }
-
-        console.log('Access Token: ' + conn.accessToken);
-        console.log('Instance URL: ' + conn.instanceUrl);
-        console.log('User ID: ' + userInfo.id);
-        console.log('Org ID: ' + userInfo.organizationId);
-
-        req.session.accessToken = conn.accessToken;
-        req.session.instanceUrl = conn.instanceUrl;
-        res.redirect('/accounts');
+app.get('/oauth/callback', function (req, res) {
+  SFConnect.oauthCallback(req.query.code)
+    .then(({accessToken, instanceUrl}) => {
+      console.log(accessToken, instanceUrl)
+      req.session.accessToken = accessToken;
+      req.session.instanceUrl = instanceUrl;
+      res.redirect('/accounts');
     });
 });
 
 app.get('/accounts', (req, res) => {
   console.log(req.session.accessToken, req.session.instanceUrl);
   res.end('hello world');
-  // query.getAccounts = ''
+  SFQuery.getAccounts(req.session.accessToken, req.session.instanceUrl)
+    .then(console.log);
 });
 
 
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT || 8080);
 console.log('server listening on port 8080');
 
 module.exports = app;
--- services/salesforce/oauth.js
+++ services/salesforce/oauth.js
@@ -1,8 +1,37 @@
 const jsforce = require('jsforce');
 
 // Salesforce OAuth2 client information
-module.exports = new jsforce.OAuth2({
-    clientId: process.env.SF_CLIENT_ID,
-    clientSecret: process.env.SF_CLIENT_SECRET,
-    redirectUri: process.env.AUTH_CALLBACK_URL
+const oauth2 = new jsforce.OAuth2({
+  loginUrl: 'https://login.salesforce.com',
+  clientId: process.env.SF_CLIENT_ID,
+  clientSecret: process.env.SF_CLIENT_SECRET,
+  redirectUri: process.env.AUTH_CALLBACK_URL
 });
+
+// Login to SF with OAuth2 token
+const oauthCallback = queryCode => {
+  const conn = new jsforce.Connection({
+    oauth2: oauth2
+  });
+  //var code = req.query.code;
+  return new Promise((resolve, reject) => {
+    conn.authorize(queryCode, (err, userInfo) => {
+      if (err) {
+        return resolve(err);
+      }
+    console.log('Access Token: ' + conn.accessToken);
+    console.log('Instance URL: ' + conn.instanceUrl);
+    console.log('User ID: ' + userInfo.id);
+    console.log('Org ID: ' + userInfo.organizationId);
+    resolve({
+      accessToken: conn.accessToken, 
+      instanceUrl: conn.instanceUrl
+    });
+    });
+  });
+};
+
+module.exports = {
+  oauth2,
+  oauthCallback
+};
